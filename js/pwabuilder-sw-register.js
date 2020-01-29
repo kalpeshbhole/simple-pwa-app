94ee3b071d5c84802bbf537705eb4b6e3f07badf
kbhole <kbhole@legalzoom.com>
2020-01-30 01:33:16 +0530

    additional functions

--- js/pwabuilder-sw-register.js
+++ js/pwabuilder-sw-register.js
@@ -17,3 +17,40 @@ if ("serviceWorker" in navigator) {
       });
   }
 }
+
+let deferredPrompt = null;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+});
+
+async function install() {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    console.log(deferredPrompt)
+    deferredPrompt.userChoice.then(function(choiceResult){
+      if (choiceResult.outcome === 'accepted') {
+      console.log('Your PWA has been installed');
+    } else {
+      console.log('User chose to not install your PWA');
+    }
+    deferredPrompt = null;
+    });
+  }
+}
+
+function getLocation() {
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position.coords);
+
+      return position;
+    })
+  }
+  else {
+    console.info("geolocation is not supported in this environment");
+  }
+}
